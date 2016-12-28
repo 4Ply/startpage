2aaaa427a28f2715a0ce0988aa187191b88e199f
Pawel Maskowicz <gamer.4ply@gmail.com>
2016-12-28 17:59:20 +0200

    Fix navigation bug causing paste handler to navigate to incorrect add_anime URL

--- js/add_anime.js
+++ js/add_anime.js
@@ -17,7 +17,7 @@ function addAnime(suffix, customName, season) {
         dataType: "json",
         cache: false,
         success: function (response) {
-            window.location = "../";
+            window.location = "../startpage";
         },
         error: function (xhr, textStatus, errorThrown) {
             console.log("error : " + textStatus);
